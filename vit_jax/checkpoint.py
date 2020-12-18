f4bae8c68e1889fea299c7229216ca0134d4c4da
jminhh <46009391+jminhh@users.noreply.github.com>
2020-12-18 23:43:33 +0900

    Update checkpoint.py

--- vit_jax/checkpoint.py
+++ vit_jax/checkpoint.py
@@ -34,8 +34,8 @@ def _flatten_dict(d, parent_key='', sep='/'):
       items.extend(_flatten_dict(v, path, sep=sep).items())
     else:
       items.append((path, v))
-
-  # Keeps the empty dict if it was set explicitly.
+      
+  # 빈 딕셔너리가 명시적으로 설정되어 있으면 유지
   if parent_key and not d:
     items.append((parent_key, {}))
 
@@ -48,14 +48,14 @@ def inspect_params(*,
                    logger,
                    fail_if_extra=True,
                    fail_if_missing=True):
-  """Inspects whether the params are consistent with the expected keys."""
+  """매개변수가 예상 키와 일치하는지 검사"""
   params_flat = _flatten_dict(params)
   expected_flat = _flatten_dict(expected)
   missing_keys = expected_flat.keys() - params_flat.keys()
   extra_keys = params_flat.keys() - expected_flat.keys()
 
-  # Adds back empty dict explicitly, to support layers without weights.
-  # Context: FLAX ignores empty dict during serialization.
+  # 가중치 없는 계층을 지원하기 위해 뒤에 빈 딕셔너리 명시적으로 추가
+  # Context: FLAX는 직렬화 과정에서 빈 딕셔너리 무시
   empty_keys = set()
   for k in missing_keys:
     if isinstance(expected_flat[k], dict) and not expected_flat[k]:
@@ -79,19 +79,17 @@ def inspect_params(*,
 
 
 def recover_tree(keys, values):
-  """Recovers a tree as a nested dict from flat names and values.
+  """트리를 1차원 이름과 값에서 중첩된 딕셔너리로 복구한다.
 
-  This function is useful to analyze checkpoints that are without need to access
-  the exact source code of the experiment. In particular, it can be used to
-  extract an reuse various subtrees of the scheckpoint, e.g. subtree of
-  parameters.
+  이 함수는 정확한 소스코드에 액세스할 필요 없는 체크포인트를 분석하는 것에 유용하다.
+  특히 체크포인트의 다양한 하위트리(예: 매개변수의 하위트리)를 재사용할 수 있다.
 
   Args:
-    keys: a list of keys, where '/' is used as separator between nodes.
-    values: a list of leaf values.
+    keys: 키들로 구성된 리스트, '/'는 노드 사이 분리기로 사용된다.
+    values: 최하위 노드(잎)의 값으로 구성된 리스트.
 
   Returns:
-    A nested tree-like dict.
+    중첩된 트리형의 딕셔너리
   """
   tree = {}
   sub_trees = collections.defaultdict(list)
@@ -108,7 +106,7 @@ def recover_tree(keys, values):
 
 
 def _traverse_with_names(tree):
-  """Traverses nested dicts/dataclasses and emits (leaf_name, leaf_val)."""
+  """중첩된 딕셔너리/데이터클래스들을 통과하고 (leaf_name, leaf_val)을 내보낸다."""
   if dataclasses.is_dataclass(tree):
     tree = flax.serialization.to_state_dict(tree)
   if isinstance(tree, dict):
@@ -121,52 +119,48 @@ def _traverse_with_names(tree):
 
 
 def tree_flatten_with_names(tree):
-  """Populates tree_flatten with leaf names.
+  """tree_flatten을 leaf 이름으로 채운다.
 
-  This function populates output of tree_flatten with leaf names, using a
-  custom traversal that produces names is provided. The custom traversal does
-  NOT have to traverse tree in the same order as jax, as we take care of
-  automatically aligning jax' and custom traversals.
+  이 함수는 tree_flatten의 출력을 leaf 이름으로 채운다. 이름을 생성하는 사용자 지정 순회가 제공된다. 
+  jax' 및 사용자 지정 순회를 자동으로 정렬하기 때문에 사용자 지정 순회는 jax와 같은 순서로 트리를 순회할 필요가 없다.
 
   Args:
     tree: python tree.
 
   Returns:
-    A list of values with names: [(name, value), ...]
+    이름과 값으로 이루어진 리스트: [(name, value), ...]
   """
   vals, tree_def = jax.tree_flatten(tree)
 
-  # "Fake" token tree that is use to track jax internal tree traversal and
-  # adjust our custom tree traversal to be compatible with it.
+  # jax 내부 트리 순회를 추적하고 이 트리 순회와 호환되도록 사용자 지정 트리 순회를 조정하는 데에 사용되는 "Fake" 토큰 트리
   tokens = range(len(vals))
   token_tree = tree_def.unflatten(tokens)
   val_names, perm = zip(*_traverse_with_names(token_tree))
   inv_perm = np.argsort(perm)
-
-  # Custom traverasal should visit the same number of leaves.
+  
+  # 사용자 지정 순회는 동일한 수의 leaf을 방문해야 한다.
   assert len(val_names) == len(vals)
 
   return [(val_names[i], v) for i, v in zip(inv_perm, vals)], tree_def
 
 
 def save(data, path):
-  """Util for checkpointing: saves jax pytree objects to the disk.
+  """체크포인트 작성에 사용: jax pytree 개체를 디스크에 저장합니다.
 
-  These checkpoints can later be recovered with `load()`.
-
+  이러한 체크포인트는 나중에 `load()`를 사용하여 복구할 수 있다.
+  
   Args:
-    data: arbitrary jax pytree to be saved.
-    path: a path to save the data.
+    data: 저장할 임의의 jax pytree
+    path: 데이터를 저장할 경로
   """
   names_and_vals, _ = tree_flatten_with_names(data)
   io_buffer = io.BytesIO()
 
-  # savez uses `seek()` API call, which is not supported by cns. Thus, we first
-  # write the checkpoint to the temp buffer and then write it to the disk.
+  # savez는 cns에서 제공하지 않는 `seek()` API를 쓴다.
+  # 따라서 먼저 체크포인트를 temp 버퍼에 쓰고난 다음 디스크에 쓴다.
   np.savez(io_buffer, **{k: v for k, v in names_and_vals})
 
-  # In order to be robust to interruptions we first save checkpoint to the
-  # temporal file and then move to actual path name.
+  # 인터럽트에 대응을 하기 위해 먼저 체크포인트를 임시 파일에 저장한 다음 실제 경로로 이동시킨다.
   path_tmp = path + '-TEMPORARY'
   gfile.makedirs(os.path.dirname(path))
   with gfile.GFile(path_tmp, 'wb') as f:
@@ -175,7 +169,7 @@ def save(data, path):
 
 
 def load(path):
-  """Loads params from a checkpoint previously stored with `save()`."""
+  """`save()`로 먼저 저장해둔 체크포인트로부터 파라미터를 가져온다."""
   with gfile.GFile(path, 'rb') as f:
     ckpt_dict = np.load(f, allow_pickle=False)
     keys, values = zip(*list(ckpt_dict.items()))
@@ -183,19 +177,16 @@ def load(path):
 
 
 def load_pretrained(*, pretrained_path, init_params, model_config, logger):
-  """Loads/converts a pretrained checkpoint for fine tuning.
+  """fine tuning을 위해 미리 학습된 체크포인트를 가져오고 변환한다.
   
   Args:
-    pretrained_path: File pointing to pretrained checkpoint.
-    init_params: Parameters from model. Will be used for the head of the model
-      and to verify that the model is compatible with the stored checkpoint.
-    model_config: Configuration of the model. Will be used to configure the
-      head and rescale the position embeddings.
-    logger: Logger to use to output diagnostic messages.
-
+    pretrained_path: 미리 학습된 체크포인트를 가리키는 파일
+    init_params: 모델의 파라미터. 모델의 헤드로 사용되고 그 모델이 저장된 체크포인트와 호환되는지 확인한다.
+    model_config: 모델의 구성. 헤드를 구성하고 위치 임베딩의 크기를 조정하는 데 사용한다.
+    logger: 진단 메시지 출력에 사용할 logger
+    
   Returns:
-    Parameters like `init_params`, but loaded with pretrained weights from
-    `pretrained_path` and adapted accordingly.
+    'init_params'와 같은 매개변수지만, 'pretrained_path'에서 미리 학습된 가중치를 로드하고 그에 따라 조정된다.
   """
 
   restored_params = inspect_params(
@@ -204,9 +195,8 @@ def load_pretrained(*, pretrained_path, init_params, model_config, logger):
       logger=logger,
       fail_if_extra=False,
       fail_if_missing=False)
-
-  # The following allows implementing fine-tuning head variants depending on the
-  # value of `representation_size` in the fine-tuning job:
+  
+  # 다음은 fine-tuning 작업에서 `representation_size` 값에 따라 fine-tuning 헤드 변형을 구현할 수 있도록 한다.
   # - `None` : drop the whole head and attach a nn.Linear.
   # - same number as in pre-training means : keep the head but reset the last
   #    layer (logits) for the new task.
@@ -218,7 +208,7 @@ def load_pretrained(*, pretrained_path, init_params, model_config, logger):
   restored_params['head']['bias'] = init_params['head']['bias']
 
   if 'posembed_input' in restored_params.get('Transformer', {}):
-    # Rescale the grid of position embeddings. Param shape is (1,N,1024)
+    # 위치 임베딩의 그리드를 재조정한다. 파라미터 모양은 (1,N,1024)
     posemb = restored_params['Transformer']['posembed_input']['pos_embedding']
     posemb_new = init_params['Transformer']['posembed_input']['pos_embedding']
     if posemb.shape != posemb_new.shape:
